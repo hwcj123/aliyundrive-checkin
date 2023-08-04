adf90bd826e7bbc2d5ed8116debdbe0b9ec61e14
hwcj123 <1096037785@qq.com>
2023-08-04 23:57:31 +0800

    Update aliyundrive.py

--- aliyundrive.py
+++ aliyundrive.py
@@ -36,7 +36,7 @@ class Aliyundrive:
             if not flag:
                 return handle_error(f'check_in error: {message}')
             
-            flag, message = self._get_reward_for_month(access_token, signin_count)
+            flag, message = self._get_reward_for_month(access_token)
             print("month",flag, message)
             if not flag:
                 return handle_error(f'_get_reward_for_month error: {message}')
@@ -135,7 +135,7 @@ class Aliyundrive:
     :return tuple[1]: message 奖励信息或者出错信息
     """
     @retry(stop=stop_after_attempt(3), wait=wait_fixed(1))
-    def _get_reward_for_month(self, access_token: str, sign_day: int) -> tuple[bool, str]:
+    def _get_reward_for_month(self, access_token: str) -> tuple[bool, str]:
         url = 'https://member.aliyundrive.com/v1/activity/sign_in_reward'
         params = {'_rx-s': 'mobile'}
         headers = {'Authorization': f'Bearer {access_token}'}
